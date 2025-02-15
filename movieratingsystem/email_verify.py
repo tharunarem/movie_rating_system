5e8a726ee7d97cba361969f325a9797c866d8e4e
tharunarem <aremtharun824@gmail.com>
2025-02-15 14:24:46 +0530

    Updated files

--- movieratingsystem/email_verify.py
+++ movieratingsystem/email_verify.py
@@ -1,6 +1,6 @@
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS=True
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='rm.rm5.rm6@gmail.com'
-EMAIL_HOST_PASSWORD='tdfc pzlv vdoz vevn'
+EMAIL_HOST_USER='enter domain email'
+EMAIL_HOST_PASSWORD='enter your pass'
 EMAIL_PORT=587
